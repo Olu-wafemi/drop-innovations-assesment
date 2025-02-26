e4db6aedd143e61e18364ba0ad3e3fa840ba12f4
Oluwafemi <femio82@gmail.com>
2025-02-26 17:15:06 +0100

    fixed all issues

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import cors from "cors"
 import helmet from "helmet"
 import routes from "./routes/index"
@@ -18,6 +18,9 @@ app.use(express.json())
 app.use(cors());
 app.use(helmet())
 app.use("/api", routes)
+app.get("/", (req:Request, res: Response) => {
+    res.json({ message: "Welcome to the API" });
+});
 
 
 export default app;
